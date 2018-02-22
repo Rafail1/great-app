ac6fa9d55019146d5970b1cf8dc71dfa5c16ba1c
Rafail1 <ya.rafon-92@ya.ru>
2018-02-22 20:31:50 +0300

    First

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,23 @@
+///<reference path="../../../node_modules/rxjs/add/operator/map.d.ts"/>
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {AuthenticationService} from './authentication.service';
+import {Observable} from 'rxjs/Observable';
+import {User} from '../_models/user';
+
+
+@Injectable()
+export class UserService {
+    constructor(
+        private httpClient: HttpClient,
+        private authenticationService: AuthenticationService) {
+    }
+
+    getUsers(): Observable<User[]> {
+        // add authorization header with jwt token
+        const headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authenticationService.token });
+
+        return this.httpClient.get<User[]>('/api/users', { headers: headers });
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {AuthenticationService} from './_services/authentication.service';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import {UserService} from './_services/user.service';
+import {AuthGuard} from './_guards/auth-guard';
+import { routing } from './app.routing';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        routing
+    ],
+    providers: [
+        AuthGuard,
+        AuthenticationService,
+        UserService,
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
